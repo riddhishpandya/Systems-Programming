1927c8f47ce1f0dbc0a5806272699b402abd3b29
riddhishpandya <riddhishpandya@gmail.com>
2016-10-13 01:01:35 -0400

    inital mymalloc.c

---
+++ mymalloc.c
@@ -0,0 +1,28 @@
+/*
+ * mymalloc.c
+ */
+#include <stdio.h>
+
+
+
+static char myblock[5000];
+void * ptr = &myblock;
+
+
+void* malloc (size_t size) {
+
+	/* find the next free block that is greater than requested size */
+	
+	return;
+}
+
+void free (void* ptr) {
+	return;
+}
+
+int main( int number_of_args, char* arg_list[] ) {
+ 
+    unsigned short size_t;
+    printf("pointer is of size: %ld\n", sizeof(size_t));
+}
+
