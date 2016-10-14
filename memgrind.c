da3d91cae058ef6f553069d5f0a69f65aa52253d
riddhishpandya <riddhishpandya@gmail.com>
2016-10-13 20:20:53 -0400

    Second instance of memgrind.c

--- memgrind.c
+++ memgrind.c
@@ -5,34 +5,62 @@
 
 
 void testA(){
-
-	int counter = 0;
 	char* malloc_pointers[3000];
+	int pointer_counter = 0;
+	int workload_counter = 0;
+	long int time_total = 0;
 
-	struct timeval start, end;
-  	gettimeofday(&start, NULL);
+	while(workload_counter < 100) {
+		struct timeval start, end;
+		gettimeofday(&start, NULL);
 
-  	while(counter < 3000) {
 
-  		malloc_pointers[counter] = malloc(1);
-  		counter++;
+  		while(pointer_counter < 3000) {
 
-  	}
+	  		malloc_pointers[pointer_counter] = malloc(1);
+	  		pointer_counter++;
 
-  	while(counter < 3000) {
+	  	}
+	  	pointer_counter = 0;
+	  	while(pointer_counter < 3000) {
 
-  		free(malloc_pointers[counter]);
-  		counter++;
+	  		free(malloc_pointers[pointer_counter]);
+	  		pointer_counter++;
 
-  	}
- 	
-
-  	gettimeofday(&end, NULL);
-  	printf("Time Required = %ld microseconds\n", ((end.tv_sec * 1000000 + end.tv_usec)
-	- (start.tv_sec * 1000000 + start.tv_usec)));	
+	  	}
+	  	gettimeofday(&end, NULL);
+	  	time_total = time_total + ((end.tv_sec * 1000000 + end.tv_usec)
+		- (start.tv_sec * 1000000 + start.tv_usec));
+		workload_counter++;
+	}
+	long int average_time = time_total/100;
+	printf("Average Time for Test A was %ld microseconds\n", average_time);	
 }
 
 void testB(){
+	int pointer_counter = 0;
+	int workload_counter = 0;
+	long int time_total = 0;
+
+	while(workload_counter < 100) {
+		struct timeval start, end;
+		gettimeofday(&start, NULL);
+
+
+  		while(pointer_counter < 3000) {
+
+	  		char* test_pointer = (char*)malloc(1);
+	  		free(test_pointer);
+	  		pointer_counter++;
+	  	}
+
+	  	gettimeofday(&end, NULL);
+	  	time_total = time_total + ((end.tv_sec * 1000000 + end.tv_usec)
+		- (start.tv_sec * 1000000 + start.tv_usec));
+		workload_counter++;
+	}
+	long int average_time = time_total/100;
+	printf("Average Time for Test B was %ld microseconds\n", average_time);	
 
 }
 
@@ -50,8 +78,9 @@ void testEF(){
 
 
 int main( int number_of_args, char* arg_list[] ) {
-
-
+	block_init();
+	testA();
+	testB();
     
     return 0;
 }
